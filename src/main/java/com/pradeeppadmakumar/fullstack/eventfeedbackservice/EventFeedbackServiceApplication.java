bef372e4d01ae7cedb485ccbde0624229b6f848d
Pradeep Padmakumar <pradeep.padmakumar91@gmail.com>
2023-08-08 11:16:38 -0500

    Initial setup of Angular and SpringBoot

---
+++ src/main/java/com/pradeeppadmakumar/fullstack/eventfeedbackservice/EventFeedbackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pradeeppadmakumar.fullstack.eventfeedbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFeedbackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFeedbackServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradeeppadmakumar/fullstack/eventfeedbackservice/EventFeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeeppadmakumar.fullstack.eventfeedbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
